d78a7f52777977362cfaae028c8441a0a2856eb6
AllenChou <f1470891079@gmail.com>
2026-02-05 16:31:07 +0800

    🔧 fix(cache): Refactor default datasets cache path for consistency
    Update the formatting of the default datasets cache path to enhance readability and maintain consistency with the existing cache structure. This change does not alter the functionality but improves code clarity.

--- mdc_ds/__init__.py
+++ mdc_ds/__init__.py
@@ -11,6 +11,8 @@ MDC_DATASETS_CACHE_NAME: Final[str] = "MDC_DATASETS_CACHE"
 DEFAULT_MDC_DOWNLOADS_CACHE: Final[Path] = Path(
     "~/.cache/huggingface/datasets/downloads"
 ).expanduser()
-DEFAULT_MDC_DATASETS_CACHE: Final[Path] = Path("~/.cache/huggingface/datasets").expanduser()
+DEFAULT_MDC_DATASETS_CACHE: Final[Path] = Path(
+    "~/.cache/huggingface/datasets"
+).expanduser()
 
 __all__ = ["get_dataset"]
